b662c6d2de0169471e2fc3dd789491eda08c4466
TechSavvyDivyansh <divyanshmodi2510@gmail.com>
2024-05-07 23:56:07 +0530

    kruskal

--- DAA/Kruskal.c
+++ DAA/Kruskal.c
@@ -1,75 +1,84 @@
-// kruskals
-
 #include <stdio.h>
-#include <stdlib.h>
-int i, j, k, a, b, u, v, n=9, ne = 1;
-int cost[50][50]={
-    {0,4,0,0,0,0,0,8,0},
-    {4,0,8,0,0,0,0,11,0},
-    {0,8,0,7,0,4,0,0,2},
-    {0,0,7,0,9,14,0,0,0},
-    {0,0,0,9,0,10,0,0,0},
-    {0,0,4,14,10,0,2,0,0},
-    {0,0,0,0,0,2,0,1,6},
-    {8,11,0,0,0,0,1,0,7},
-    {0,0,2,0,0,0,6,7,0},
+
+struct Edge {
+	int src, dest, weight;
 };
-int min, mincost = 0, parent[10]={0};
-int find(int);
-int uni(int, int);
-int main()
-{
-    printf("\n\tImplementation of Kruskal's algorithm\n");
-    // printf("\nEnter the no. of vertices:");
-    // scanf("%d", &n);
-    // printf("\nEnter the cost adjacency matrix:\n");
-    for (i = 1; i <= n; i++)
-    {
-        for (j = 1; j <= n; j++)
-        {
-            // scanf("%d", &cost[i][j]);
-            if (cost[i][j] == 0)
-                cost[i][j] = 999;
-        }
-    }
-    printf("The edges of Minimum Cost Spanning Tree are\n");
-    while (ne < n)
-    {
-        for (i = 1, min = 999; i <= n; i++)
-        {
-            for (j = 1; j <= n; j++)
-            {
-                if (cost[i][j] < min)
-                {
-                    min = cost[i][j];
-                    a = u = i;
-                    b = v = j;
-                }
-            }
-        }
-        u = find(u);
-        v = find(v);
-        if (uni(u, v))
-        {
-            printf("%d edge (%d,%d) =%d\n", ne++, a, b, min);
-            mincost += min;
-        }
-        cost[a][b] = cost[b][a] = 999;
-    }
-    printf("\n\tMinimum cost = %d\n", mincost);
+
+int adj[100][100], n, e;
+struct Edge edges[100], mst[100];
+
+void sortEdges() {
+	for (int i = 0; i < e - 1; i++) {
+		for (int j = 0; j < e - i - 1; j++) {
+			if (edges[j].weight > edges[j + 1].weight) {
+				struct Edge temp = edges[j];
+				edges[j] = edges[j + 1];
+				edges[j + 1] = temp;
+			}
+		}
+	}
 }
-int find(int i)
-{
-    while (parent[i])
-        i = parent[i];
-    return i;
+
+int findSet(int parent[], int i) {
+	if (parent[i] == -1) {
+		return i;
+	}
+	return findSet(parent, parent[i]);
 }
-int uni(int i, int j)
-{
-    if (i != j)
+
+void Union(int parent[], int x, int y) {
+	int xset = findSet(parent, x);
+	int yset = findSet(parent, y);
+	parent[xset] = yset;
+}
+
+void kruskal() {
+	int parent[n];
+	for (int i = 0; i < n; i++) {
+		parent[i] = -1;
+	}
+	int mstIndex = 0;
+	for (int i = 0; i < e; i++) {
+		int x = findSet(parent, edges[i].src);
+		int y = findSet(parent, edges[i].dest);
+		if (x != y) {
+			mst[mstIndex++] = edges[i];
+			Union(parent, x, y);
+		}
+	}
+	printf("Minimum Spanning Tree:\n");
+	for (int i = 0; i < n - 1; i++) 
     {
-        parent[j] = i;
-        return 1;
-    }
-    return 0;
+		printf("%c -> %c: %d\n", 'a' + mst[i].src, 'a' + mst[i].dest, mst[i].weight);
+	}
+}
+
+int main() {
+	n = 9; // Number of vertices
+	// Hardcoded adjacency matrix
+	int adj[9][9] = {
+		{0, 4, 0, 0, 0, 0, 0, 8, 0},
+		{4, 0, 8, 0, 0, 0, 0, 11, 0},
+		{0, 8, 0, 7, 0, 4, 0, 0, 2},
+		{0, 0, 7, 0, 9, 14, 0, 0, 0},
+		{0, 0, 0, 9, 0, 10, 0, 0, 0},
+		{0, 0, 4, 14, 10, 0, 2, 0, 0},
+		{0, 0, 0, 0, 0, 2, 0, 1, 6},
+		{8, 11, 0, 0, 0, 0, 1, 0, 7},
+		{0, 0, 2, 0, 0, 0, 6, 7, 0}
+	};
+	e = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (adj[i][j] != 0) {
+				edges[e].src = i;
+				edges[e].dest = j;
+				edges[e].weight = adj[i][j];
+				e++;
+			}
+		}
+	}
+	sortEdges();
+	kruskal();
+	return 0;
 }
