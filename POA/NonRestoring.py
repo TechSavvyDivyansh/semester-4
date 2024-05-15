95be066a686d22e1fe36b253e5cd6ae780d29884
TechSavvyDivyansh <divyanshmodi2510@gmail.com>
2024-05-15 13:01:55 +0530

    non restoring edited. now it works for all test cases

--- POA/NonRestoring.py
+++ POA/NonRestoring.py
@@ -1,8 +1,19 @@
-def DecToBin(n, bitsize):
-    replacedBin=[]
-    for i in range(bitsize):
-        replacedBin.append(n%2)
-        n//=2
+def DecToBin(n, qval):
+    global count
+    replacedBin = []
+    
+    #for Q qval=0 and for M qval=1
+
+    if qval == 0:
+        size = n.bit_length()     #if Q=50  binary->[1,1,0,0,1,0]     size=count=6
+        count = size
+    else:
+        size = count + 1          #if M=5   binary->[0,0,0,0,1,0,1]   size should be count+1=7
+    
+    for i in range(size):
+        replacedBin.append(n % 2)
+        n //= 2
+    
     return replacedBin[::-1]
 
 
@@ -36,7 +47,7 @@ def comp(Mu):
             Mu_copy[i] = 1
         else:
             Mu_copy[i] = 0
-    return add(Mu_copy, [0, 0, 0, 0, 1])
+    return add(Mu_copy, [0]*count+[1])
 
 
 def shiftLeft(A,Q):
@@ -60,27 +71,30 @@ def non_restore(A,Q,M,n):
 
         if(A[0]==0):
             A,Q=shiftLeft(A,Q)
-            print(f'After Shift Left=> A:{A},Q:{Q}')
+            # print(f'After Shift Left=> A:{A},Q:{Q}')
             A=add(A,compl)
-            print(f'After A-M=> A:{A},Q:{Q}')
+            # print(f'After A-M=> A:{A},Q:{Q}')
 
         else:
             A,Q=shiftLeft(A,Q)
-            print(f'After Shift left=> A:{A},Q:{Q}')
+            # print(f'After Shift left=> A:{A},Q:{Q}')
             A=add(A,Main)     
-            print(f'After A+M=> A:{A},Q:{Q}')
+            # print(f'After A+M=> A:{A},Q:{Q}')
 
         if(A[0]==0):
-            Q[3]=1
-            print(f'After Q[3]=1 addition => A:{A},Q:{Q}')
+            Q[-1]=1
+            # print(f'After Q[-1]=1 addition => A:{A},Q:{Q}')
             
         else:
-            Q[3]=0       
-            print(f'After Q[3]=0 addition => A:{A},Q:{Q}')
+            Q[-1]=0       
+            # print(f'After Q[-1]=0 addition => A:{A},Q:{Q}')
         n-=1
     
     if(A[0]==1):
         A=add(A,M)
+
+
+        
     print(f'\n Quotient is:{Q} and remainder is:{A}')
     remainder,quotient=BinaryToDec(A,Q)
     print(f'Quotient:{quotient} and remainder is:{remainder}')
@@ -92,10 +106,10 @@ def non_restore(A,Q,M,n):
 
 
 q=int(input("Enter dividend:"))
-Q=DecToBin(q,4)
+Q=DecToBin(q,0)
 m=int(input("Enter divisor:"))
-M=DecToBin(m,5)
-A=[0,0,0,0,0]
+M=DecToBin(m,1)
+A = [0] * (count + 1) 
 
 n=len(Q)
 non_restore(A,Q,M,n)
